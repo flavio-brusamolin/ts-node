1e69cd3ec1e9983134b24d3c91dacc251e8f07f2
Flavio Brito <flavio.brito@leucotron.com.br>
2019-12-10 17:37:50 -0300

    Application structure

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express'
+
+import User from '../models/User'
+
+class UserController {
+  public async list (req: Request, res: Response): Promise<Response> {
+    const users = await User.find()
+
+    return res.status(200).json({
+      success: true,
+      users
+    })
+  }
+
+  public async store (req: Request, res: Response): Promise<Response> {
+    const { email, firstName, lastName } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: 'User email already exists'
+      })
+    }
+
+    await User.create({
+      email,
+      firstName,
+      lastName
+    })
+
+    return res.status(201).json({
+      success: true,
+      message: 'Successful registration'
+    })
+  }
+}
+
+export default new UserController()
